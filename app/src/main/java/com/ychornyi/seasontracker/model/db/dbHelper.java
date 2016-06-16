70c54319a943ad62020fa502f92286309837ff72
y.chornyi <xym4er@gmail.com>
2016-06-16 11:39:55 +0300

    123

--- app/src/main/java/com/ychornyi/seasontracker/model/db/dbHelper.java
+++ app/src/main/java/com/ychornyi/seasontracker/model/db/dbHelper.java
@@ -1,31 +1,67 @@
 package com.ychornyi.seasontracker.model.db;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.ychornyi.seasontracker.model.items.SeriesItem;
 
 /**
  * Created by y.chornyi on 15.06.2016.
  */
 public class dbHelper extends SQLiteOpenHelper {
-    public static final String DB_NAME = "Seasons";
-    public static final int VERSION = 1;
-    public static final String  = "";
-    public static final String  = "";
-    public static final String  = "";
-    public static final String  = "";
+    public static final String DATABASE_NAME = "main.db";
+    public static final int DATABASE_VERSION = 1;
 
-    public dbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    private static final String SERIALS_TABLE_NAME = "serials";
+    private static final String SERIALS_COLUMN_ID = "_id";
+    private static final String SERIALS_COLUMN_NAME = "name";
+    private static final String SERIALS_COLUMN_LAST_UPDATE = "last_update";
+
+
+
+    public dbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table "+ SERIALS_TABLE_NAME +" ("+
+        SERIALS_COLUMN_ID+" integer primary key autoincrement, "+
+        SERIALS_COLUMN_NAME+" text not null, "+
+        SERIALS_COLUMN_LAST_UPDATE+" text);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXIST "+ SERIALS_TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addSeries (SeriesItem series){
+        boolean flag = false;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = null;
+        String[] columns = null;
+        columns = new String[]{ SERIALS_COLUMN_NAME };
+        cursor = db.query(SERIALS_TABLE_NAME,columns,null,null,null,null,null);
+        if (cursor!=null){
+            if (cursor.moveToFirst()){
+                do {
+                    if (cursor.getString(cursor.getColumnIndex(SERIALS_COLUMN_NAME)).toLowerCase().equals(series.getName().toLowerCase())){
+                        flag = true;
+                        break;
+                    }
+                } while (cursor.moveToNext());
+            }
+        }
+        if (flag){
+
+        }else{
+
+        }
+        return !flag;
 
     }
 }
--- app/src/main/java/com/ychornyi/seasontracker/model/items/SeriesItem.java
+++ app/src/main/java/com/ychornyi/seasontracker/model/items/SeriesItem.java
@@ -5,8 +5,57 @@ package com.ychornyi.seasontracker.model.items;
  */
 public class SeriesItem {
     private int id;
+    private String name;
     private int season;
     private int seria;
     private int date;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeason() {
+        return season;
+    }
+
+    public void setSeason(int season) {
+        this.season = season;
+    }
+
+    public int getSeria() {
+        return seria;
+    }
+
+    public void setSeria(int seria) {
+        this.seria = seria;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public SeriesItem(int id, String name, int season, int seria, int date) {
+
+        this.id = id;
+        this.name = name;
+        this.season = season;
+        this.seria = seria;
+        this.date = date;
+    }
 }
